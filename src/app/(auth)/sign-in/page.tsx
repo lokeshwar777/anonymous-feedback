630e611c4b5d79dd607c299675c557e7a50b6dd9
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-06-03 22:44:26 +0530

    feat: Add sign-up page and implement Toaster using ShadCN

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,25 +1,7 @@
-'use client'
-import { useSession, signIn, signOut } from 'next-auth/react'
+import React from 'react'
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button
-        className="bg-teal-500 px-3 py-1 m-4 rounded"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
-  )
+function Page() {
+  return <div>Sign in page</div>
 }
+
+export default Page
