d4f88f90ec4e3a563988946f6ec9a8c70ec39a33
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-06-22 21:13:07 +0530

    feat: Update sign-in page with form validation and authentication logic

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,121 @@
-import React from 'react'
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import Link from 'next/link'
+import { useToast } from '@/components/ui/use-toast'
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
+import { signInSchema } from '@/schemas/signInSchema'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Loader2 } from 'lucide-react'
+import { signIn } from 'next-auth/react'
 
-function Page() {
-  return <div>Sign in page</div>
+const Page = () => {
+  const { toast } = useToast()
+  const router = useRouter()
+
+  // zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: ''
+    }
+  })
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password
+    })
+
+    if (result?.error) {
+      if (result.error === 'CredentialSignin') {
+        toast({
+          title: 'Login Failed',
+          description: 'Incorrect username or password',
+          variant: 'destructive'
+        })
+      } else {
+        toast({
+          title: 'Error',
+          description: result.error,
+          variant: 'destructive'
+        })
+      }
+
+      // toast({
+      //   title: 'Login Failed',
+      //   description: 'Invalid username or password',
+      //   variant: 'destructive'
+      // })
+    }
+
+    if (result?.url) {
+      router.replace('dashboard')
+    }
+  }
+
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+      <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md'>
+        <div className='text-center'>
+          <h1 className='text-4xl font-extrabold tracking-tight lg:text-5xl mb-6'>
+            Join Anonymous Feedback
+          </h1>
+          <p className='mb-4'>Sign In to experience</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            <FormField
+              control={form.control}
+              name='identifier'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder='email/username' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type='password' placeholder='Password' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit'>SignIn</Button>
+          </form>
+        </Form>
+        <div className='text-center mt-4'>
+          <p>
+            Create an Account{' '}
+            <Link href='/sign-up' className='text-blue-600 hover:text-blue-800'>
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Page
